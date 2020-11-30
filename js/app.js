d4cd430df4518752398f004a2e1f80369fc2322b
Darian Chang <44990990+changmeister@users.noreply.github.com>
2020-11-30 04:10:31 -0800

    Minor Fixes

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const navSlide = () => {
     const navLinks = document.querySelectorAll('.nav-links li');
     
     // Toggle Nav
-    burger.addEventListener('click', () => {
+    burger.addEventListener('click', ()=>{
         nav.classList.toggle('nav-active');
     });
 
@@ -15,3 +15,4 @@ const navSlide = () => {
 }
 
 navSlide();
+
